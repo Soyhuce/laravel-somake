e7ff5dcb31bb9d574cb6ad3d5ba943b81fa886ba
Bastien Philippe <bastien.philippe@soyhuce.fr>
2022-05-11 09:28:27 +0200

    Don't use HandlesAuthorization when policy uses custom base class

--- resources/views/policy.blade.php
+++ resources/views/policy.blade.php
@@ -1,12 +1,15 @@
 namespace {{ $namespace }};
 
-use Illuminate\Auth\Access\HandlesAuthorization;
 @if($baseClass)
 use {{ $baseClass }};
+@else
+use Illuminate\Auth\Access\HandlesAuthorization;
 @endif
 
 class {{ $className }}@if($baseClassName) extends {{$baseClassName}}@endif
 
 {
+@if(!$baseClassName)
     use HandlesAuthorization;
+@endif
 }
--- tests/__snapshots__/files/PolicyCommandTest__it_creates_the_policy_with_custom_base_class__1.php
+++ tests/__snapshots__/files/PolicyCommandTest__it_creates_the_policy_with_custom_base_class__1.php
@@ -2,10 +2,8 @@
 
 namespace Domain\User\Policies;
 
-use Illuminate\Auth\Access\HandlesAuthorization;
 use Support\Policy;
 
 class UserPolicy extends Policy
 {
-    use HandlesAuthorization;
 }
