d8e15ce46399cce9056aaca5254332db4dda3095
Bastien Philippe <bastien.philippe@soyhuce.fr>
2023-01-13 15:13:25 +0100

    Remove generated tests

--- test-laravel/tests/Feature/Website/Videos/VideoController/ContractShowVideoTest.php
+++
@@ -1,8 +0,0 @@
-<?php declare(strict_types=1);
-
-/* @covers \App\Website\Videos\Controllers\VideoController::show */
-
-it('is successful', function (): void {
-    $this->getJson("api/videos/{$video}")
-        ->assertValidContract(200);
-});
