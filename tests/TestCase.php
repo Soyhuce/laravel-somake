d95908134ac87ca915ea0771047112e76ca343cb
Bastien Philippe <bastien.philippe@soyhuce.fr>
2025-02-24 11:17:02 +0100

    Ignore vendor deprecations

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,6 +2,7 @@
 
 namespace Soyhuce\Somake\Tests;
 
+use ErrorException;
 use Illuminate\Foundation\Testing\Concerns\InteractsWithDeprecationHandling;
 use Orchestra\Testbench\Foundation\PackageManifest;
 use Orchestra\Testbench\TestCase as Orchestra;
@@ -54,5 +55,32 @@ class TestCase extends Orchestra
     protected function expectedPath(string $file): string
     {
         return realpath(__DIR__ . "/Expected/{$file}");
+    }
+
+    protected function withoutDeprecationHandling(): static
+    {
+        if ($this->originalDeprecationHandler == null) {
+            $this->originalDeprecationHandler = set_error_handler(function (
+                $level,
+                $message,
+                $file = '',
+                $line = 0,
+            ): void {
+                if (in_array($level, [E_DEPRECATED, E_USER_DEPRECATED], true) || (error_reporting() & $level)) {
+                    // Silenced vendor errors
+                    if (str_starts_with($file, base_path('vendor/symfony/'))) {
+                        return;
+                    }
+
+                    if (str_starts_with($file, realpath(__DIR__.'/../vendor/composer/pcre'))) {
+                        return;
+                    }
+
+                    throw new ErrorException($message, 0, $level, $file, $line);
+                }
+            });
+        }
+
+        return $this;
     }
 }
